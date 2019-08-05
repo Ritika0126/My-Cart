2dd85bd8c73db1d0e39472dc535993d352226d5e
Ritika0126 <ritikagarg347@gmail.com>
2019-08-05 20:41:12 +0530

    Details of all available products

---
+++ Product.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self,id,name,price,quantity):
+        self.id=id
+        self.name=name
+        self.price=price
+        self.qty=quantity
+    
+        
+p1=Product(101,"pen",2,1)
+p2=Product(102,"bag",9,2)
+p3=Product(103,"bread",4,1)
